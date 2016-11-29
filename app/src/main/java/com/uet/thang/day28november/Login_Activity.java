90e13efe444aae5d8e9a82390a8a49932b567d64
trinhthang <trinhthang1122@gmail.com>
2016-11-29 22:10:35 +0700

    commit change

---
+++ app/src/main/java/com/uet/thang/day28november/Login_Activity.java
@@ -0,0 +1,10 @@
+package com.uet.thang.day28november;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Thang on 11/29/2016.
+ */
+
+public class Login_Activity extends AppCompatActivity {
+}
